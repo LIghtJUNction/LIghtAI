8461c5072c9dbde8a8ea793c694c912ded5c13bf
LIghtJUNction <106986785+LIghtJUNction@users.noreply.github.com>
2024-06-30 00:41:10 +0800

    111

--- start/ModleLoader.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoModelForSequenceClassification
-
-def load_model():
-    model = AutoModelForSequenceClassification.from_pretrained('ollama/gamma2-9b', num_labels=2)
-    return model
